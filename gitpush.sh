a677bf2d397fc1275cb6f7746cd64af479767270
insideman02468 <insideman02468@gmail.com>
2019-04-16 17:52:01 +0900

    First commit

--- gitpush.sh
+++ gitpush.sh
@@ -4,6 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
+git checkout work
 git add --all
 git commit -m "First commit"
-git push origin master
+git push origin work
