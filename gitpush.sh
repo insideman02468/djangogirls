5dedcdfa477ea3bc866cc4c6d624f7c57cf2355e
insideman02468 <insideman02468@gmail.com>
2019-04-16 18:34:33 +0900

    FInish HTML first

--- gitpush.sh
+++ gitpush.sh
@@ -6,5 +6,5 @@ git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
 git checkout work
 git add --all
-git commit -m "First commit"
+git commit -m "FInish HTML first"
 git push origin work
