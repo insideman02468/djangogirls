e288c9a792403add08745e236d41be615c1ee7da
insideman02468 <insideman02468@gmail.com>
2019-04-16 21:14:25 +0900

    Finish extend templete1!

--- gitpush.sh
+++ gitpush.sh
@@ -6,5 +6,5 @@ git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
 git checkout work
 git add --all
-git commit -m "Finish CSS decoration!"
+git commit -m "Finish extend templete1!"
 git push origin work
