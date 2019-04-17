598edb16c5a6c4a25efcae69f3b497b236813752
insideman02468 <insideman02468@gmail.com>
2019-04-17 11:39:18 +0900

    Finish  Homework: create comment model

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
-git checkout work
+git checkout Create_Comment_model
 git add --all
 git commit -m "Finish  Homework: create comment model"
-git push origin work
+git push origin Create_Comment_model
