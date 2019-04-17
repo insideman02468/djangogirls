56f8f872ec973e713ebd9f47a778139dfba4c9c9
insideman02468 <insideman02468@gmail.com>
2019-04-17 11:12:34 +0900

    Finish Homework: secure your website!

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
-git checkout work
+git checkout secure_your_website
 git add --all
 git commit -m "Finish Homework: secure your website!"
-git push origin work
+git push origin checkout secure_your_website
