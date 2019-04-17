48770197a4993f6a21b481528964ab8ba49bd61c
insideman02468 <insideman02468@gmail.com>
2019-04-17 10:53:05 +0900

    Finish Homework: add more to your website!

--- gitpush.sh
+++ gitpush.sh
@@ -6,5 +6,5 @@ git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
 git checkout work
 git add --all
-git commit -m "Finish Add publish button!"
+git commit -m "Finish Homework: add more to your website!"
 git push origin work
