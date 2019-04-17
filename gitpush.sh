64b87ea7f5514ca99bbb62738cd7f5c07081164c
insideman02468 <insideman02468@gmail.com>
2019-04-17 12:36:53 +0900

    Trying deploy

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
-git checkout heroku
+git checkout master
 git add --all
-git commit -m "additional files and changes for Heroku"
-git push origin heroku
+git commit -m "Trying deploy"
+git push origin master
