7047fc26bc8bb7d9d6b227253fe6be27472ef1a6
insideman02468 <insideman02468@gmail.com>
2019-04-17 11:14:52 +0900

    Start  Homework: create comment model

--- gitpush.sh
+++ gitpush.sh
@@ -6,5 +6,5 @@ git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
 git checkout work
 git add --all
-git commit -m "Finish Homework: secure your website!"
+git commit -m "Start  Homework: create comment model"
 git push origin work
