f54adcaa9d9cb81e8310d9eca260b68890468a78
insideman02468 <insideman02468@gmail.com>
2019-04-17 14:10:49 +0900

    Reset complete Homework: create comment model

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/djangogirls.git
 git status
-git checkout Create_Comment_model
+git checkout reset
 git add --all
 git commit -m "Reset complete Homework: create comment model"
-git push origin Create_Comment_model
+git push origin reset
